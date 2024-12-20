8093b714683f25abcd0a5464594f900ea4031479
Amaury <amaury.gaborit17@gmail.com>
2024-12-20 15:14:48 +0100

    correcting Date

--- Date.cpp
+++ Date.cpp
@@ -17,19 +17,19 @@ const bool Date::isValidDate(int day, int month, int year) const {
     if (month >= 1 && month <= 12) {
         int dayCount;
         switch (month) {
-            case 1 : 31; break;
-            case 2 : 28 + bissextileYear; break;
-            case 3 : 31; break;
-            case 4 : 30; break;
-            case 5 : 31; break;
-            case 6 : 30; break;
-            case 7 : 31; break;
-            case 8 : 31; break;
-            case 9 : 30; break;
-            case 10 : 31; break;
-            case 11 : 30; break;
-            case 13 : 31; break;
-            default: break;
+            case 1 : dayCount = 31; break;
+            case 2 : dayCount = 28 + bissextileYear; break;
+            case 3 : dayCount = 31; break;
+            case 4 : dayCount = 30; break;
+            case 5 : dayCount = 31; break;
+            case 6 : dayCount = 30; break;
+            case 7 : dayCount = 31; break;
+            case 8 : dayCount = 31; break;
+            case 9 : dayCount = 30; break;
+            case 10 : dayCount = 31; break;
+            case 11 : dayCount = 30; break;
+            case 13 : dayCount = 31; break;
+            default: dayCount = -1; break;
         }
         if (day < 1 || day > dayCount) {
             return false;
