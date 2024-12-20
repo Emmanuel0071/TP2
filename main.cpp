6b413946a6c6bc81fc33389f3140543ef0178878
Emmanuel <Emmanuel_Peyronnet@etu.u-bourgogne.fr>
2024-12-20 16:25:20 +0100

    Client+test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Room.h"
 #include "Reservation.h"
+#include "Client.h"
 
 int main() {
     Room chambre1(101, 1, 120.50);
@@ -12,5 +13,10 @@ int main() {
 
     Date today(20, 12, 2024);
 
+
+    Client Thomas("PE", "Emmanuel", "PEYRONNET" );
+    std::cout << "id personne"<< Thomas.getidClient() << std::endl;
+    std::cout << "nom personne"<< Thomas.getname() << std::endl;
+    std::cout << "prenom personne"<< Thomas.getnickname() << std::endl;
     return 0;
 }
